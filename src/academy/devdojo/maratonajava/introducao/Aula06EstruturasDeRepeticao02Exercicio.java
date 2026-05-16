eea04680653f3730f9b663e53569bfa2e2fcfe51
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-15 23:02:01 -0300

    feat: adding looping structures, classes practice and 1 unsolved exercice

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02Exercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02Exercicio {
+    public static void main(String[] args) {
+        // Print every pair number from 0 to 1000000
+
+        for (int i = 0; i <= 1000000; i += 2  ) {
+                System.out.println(i);
+
+        }
+    }
+}
