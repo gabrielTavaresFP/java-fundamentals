852f52ec67f15a21823bc1a1e5d2515518b25807
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-05-17 13:17:13 -0300

    feat : added arrays practice classes

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float && double 0
+        // char '\u0000' ' '
+        // boolean false
+        // String (references) null
+
+        String [] ages = new String [3];
+
+        System.out.println(ages [0]);
+        System.out.println(ages [1]);
+        System.out.println(ages [2]);
+
+    }
+}
