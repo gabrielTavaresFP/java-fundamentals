612f673c5ac1ae3df71033770965d076a9a88cc0
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-06-05 19:56:14 -0300

    feat: added OOP practice classes

--- src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/domain/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/domain/Anime.java
@@ -1,8 +1,51 @@
 package academy.devdojo.maratonajava.javacore.Csobrecargametodos.domain;
 
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.WorkersData;
+
 public class Anime {
+
+    private String name;
+    private String genre;
     private String type;
     private int episodes;
+
+    public void init(String name, String type, int episodes) {
+        this.name = name;
+        this.type = type;
+        this.episodes = episodes;
+
+    }
+
+    public void init(String name, String type, int episodes, String genre) {
+        this.init(name, type, episodes);
+        this.genre = genre;
+
+    }
+
+
+    public void print() {
+        System.out.println(this.name);
+        System.out.println(this.type);
+        System.out.println(this.episodes);
+        System.out.println(this.genre);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
     public String getType() {
         return type;
@@ -20,3 +63,5 @@ public class Anime {
         this.episodes = episodes;
     }
 }
+
+
---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Attack on Titan",  "TV", 150, "Action");
+
+        anime.print();
+    }
+}
