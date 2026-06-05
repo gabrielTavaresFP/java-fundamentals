bb3034a6941de5ed5cbe5f99cd3779d46a2ebf10
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-06-04 22:45:34 -0300

    feat: added OOP practice classes and exercises

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/domain/Anime.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.domain;
+
+public class Anime {
+    private String type;
+    private int episodes;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+}
