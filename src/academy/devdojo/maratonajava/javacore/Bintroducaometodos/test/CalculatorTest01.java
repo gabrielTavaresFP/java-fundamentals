93046c168ab9526903f285af636e0ccc997d7a05
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-06-23 21:25:23 -0300

    chore: review java oop methods

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculatorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculatorTest01.java
@@ -7,9 +7,15 @@ public class CalculatorTest01 {
     public static void main(String[] args) {
         Calculator calculator = new Calculator(); // <-- objeto
 
+
         calculator.twoNumbersSum();
         System.out.println("finishing CalculatorTest01");
         calculator.twoNumbersSubtraction();
         System.out.println("finishing CalculatorTest01");
+        calculator.changeTwoNumbers(10, 15);
+        int[] test1 = {1, 2, 3, 4, 5};
+        calculator.sumArray(test1);
+        System.out.println("finishing CalculatorTest01");
+
     }
 }
