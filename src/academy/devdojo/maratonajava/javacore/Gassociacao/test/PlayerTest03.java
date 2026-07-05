c4680ba96f9a1aff8159775008561228efaff4f5
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-07-04 23:05:41 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/test/PlayerTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/PlayerTest03.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.Gassociacao.domain.Team;
 public class PlayerTest03 {
     public static void main(String[] args) {
         Player p1 = new Player("Roberto Carlos");
-        Team t1 = new Team("Real Madrid");
+        Team t1 = new Team("Real Madri gitd");
 
         p1.setTeam(t1);
     }
