f9c8a232f16f492b025fa68bba35fe8daa49b2cc
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-07-07 00:25:59 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/test/SchoolTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/SchoolTest01.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.Gassociacao.domain.Teacher;
 public class SchoolTest01 {
     public static void main(String[] args) {
         Teacher teacher1 = new Teacher("Éder");
-        Teacher teacher2 = new Teacher("Ramon");
+        Teacher teacher2 = new Teacher("Ramon ");
 
         Teacher[] ts = {teacher1, teacher2};
 
