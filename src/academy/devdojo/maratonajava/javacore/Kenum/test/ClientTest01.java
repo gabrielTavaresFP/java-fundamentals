0b68274f9cc45ef4369166eba73ac322f8be8472
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-07-26 00:06:58 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -7,7 +7,7 @@ import academy.devdojo.maratonajava.javacore.Kenum.domain.TypeClient;
 public class ClientTest01 {
     public static void main(String[] args) {
         Client client1 = new Client("Hinata", TypeClient.PESSOA_FISICA, PaymentType.DEBITO);
-        Client client2 = new Client("Kageyama ", TypeClient.PESSOA_JURIDICA, PaymentType.CREDITO);
+        Client client2 = new Client("Kageyama", TypeClient.PESSOA_JURIDICA, PaymentType.CREDITO);
 
         System.out.println(client1);
         System.out.println(client2);
