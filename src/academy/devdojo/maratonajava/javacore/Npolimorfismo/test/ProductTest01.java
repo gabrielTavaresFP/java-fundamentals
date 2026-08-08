33d6edc4c9d8284e756fc9a62a036006865ea36e
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-08-08 01:26:39 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProductTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProductTest01.java
@@ -7,7 +7,7 @@ import academy.devdojo.maratonajava.javacore.Npolimorfismo.service.TaxCalculator
 public class ProductTest01 {
     public static void main(String[] args) {
         Computer computer = new Computer("MacOS  ", 9000);
-        Tomato tomato = new Tomato("Sicilian Tomato ", 10);
+        Tomato tomato = new Tomato("Sicilian Tomato  ", 10);
         TaxCalculator.computerTaxCalculate(computer);
         TaxCalculator.tomatoTaxCalculate(tomato);
     }
