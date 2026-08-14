4a40bdcfe535d43eecd6945c8ee25c6309ce1617
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-08-13 22:00:15 -0300

    daily commit

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -12,6 +12,7 @@ public class Aula07Arrays01 {
         System.out.println(names [0]);
         System.out.println(names [1]);
         System.out.println(names [2]);
+        System.out.println(names [2]);
 
     }
 }
