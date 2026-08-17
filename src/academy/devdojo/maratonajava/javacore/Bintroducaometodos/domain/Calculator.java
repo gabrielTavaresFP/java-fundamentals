034e69a3de92cd1cc4eff878db196b71665e3c9d
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-08-16 22:32:36 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculator.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
 
     public void printTwoNumbersDivision(double num1, double num2) {
         if (num2 == 0) {
-            System.out.println("Dont exist division per 0");
+            System.out.println("Dont exist division per 0 ");
             return;
         }
         System.out.println(num1 / num2);
