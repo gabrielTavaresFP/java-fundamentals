5d546896adbbbc0d99b6d0b5487714d826248503
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2026-09-16 22:36:40 -0300

    daily commit

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarTest01.java
@@ -6,9 +6,9 @@ public class CarTest01 {
 
     public static void main(String[] args) {
 
-        Car c1 = new Car("A   ", 280);
-        Car c2 = new Car("B    ", 290);
-        Car c3 = new Car("C    ", 255);
+        Car c1 = new Car("A", 280);
+        Car c2 = new Car("B", 290);
+        Car c3 = new Car("C", 255);
 
         Car.setSpeedCap(200);
 
